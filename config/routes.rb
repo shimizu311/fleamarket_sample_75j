22c0a67368cc7a88709b006d5cc5258cd4ba504d
miyyuk <orion.my.2@icloud.com>
2020-05-25 12:46:30 +0900

    routing to buy page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users
-  resources :items
+  resources :items do
+    member do
+      get 'buy'
+    end
+  end
 end
