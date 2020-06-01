2a2e93a2ce340b0432f2223d5330d7c150815407
miyyuk <orion.my.2@icloud.com>
2020-06-01 11:08:16 +0900

    add user registration function
    ユーザー新規登録時に必須項目を入力させる。
    ウィザード形式を採用。
    ビューの微調整。
    エラーハンドリングも実装。
    エラー文の日本語化も実装済み。

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      sign_in(:user, @user)
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
