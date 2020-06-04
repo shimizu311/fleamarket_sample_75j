95767c4093a34d2aa10d746c846545b5268436bc
miyyuk <orion.my.2@icloud.com>
2020-06-04 11:28:37 +0900

    add pay function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,9 +32,52 @@ class ItemsController < ApplicationController
   def destroy
   end
 
-  def buy
+  require "payjp"
+  def confirm
+    @item = Item.find(params[:id])
+    if current_user.card.present?
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      @card = Card.find_by(user_id: current_user.id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card = customer.cards.retrieve(@card.card_id)
+      @card_number_display = "**** **** **** " + @default_card.last4
+      @exp_month_display = @default_card.exp_month.to_s
+      @exp_year_display = @default_card.exp_year.to_s.slice(2,3)
+    else
+    end
   end
-  
+
+  def pay
+    @item = Item.find(params[:id])
+    if @item.buyer_id.present?
+      redirect_to item_path(@item.id), alert: '売り切れています'
+    else
+      @item.with_lock do
+        if current_user.card.present?
+          @card = Card.find_by(user_id: current_user.id)
+          Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+          charge = Payjp::Charge.create(
+            amount: @item.price,
+            customer: Payjp::Customer.retrieve(@card.customer_id),
+            currency: 'jpy'
+          )
+        else
+          Payjp::Charge.create(
+            amount: @item.price,
+            card: params['payjp-token'],
+            currency: 'jpy'
+          )
+        end
+        if @item.update(buyer_id: current_user.id)
+          redirect_to root_path
+        else
+          flash.now[:alert] = '購入に失敗しました'
+          render :confirm
+        end
+      end
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :damage_id, :fee_id, :area_id, :send_date_id, :price, images_attributes: [:image_url], brand_attributes: [:name]).merge(seller_id: current_user.id)
